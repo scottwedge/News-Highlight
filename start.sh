8779115feb77061a6cfd54d2b58f2d267dd59873
Soniawangui <koisonia99@gmail.com>
2019-06-26 10:06:06 +0300

    add news export

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export MOVIE_API_KEY= '1c2a6cb97eed4fb493836de92b65272d'
-export SECRET_KEY= '1234'
+from app import app
+NEWS_API_KEY= '1c2a6cb97eed4fb493836de92b65272d'
+SECRET_KEY= '1234'
 
 python3.6 manage.py server
