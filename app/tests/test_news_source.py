94d096059481f4366b274ca2c455c5bab6be3f46
swedge <scott.wedge@gmail.com>
2020-02-06 16:36:27 -0500

    Save intermediate files - still errors when try to run tests

--- app/tests/test_news_source.py
+++ app/tests/test_news_source.py
@@ -1,6 +1,11 @@
 import unittest
-from models import source 
-Source = source.Source
+import app
+import app.models
+#from app.models import source 
+from app.models import Source 
+#Source = source.Source
+from flask import render_template
+#from app import app
 
 class SourceTest(unittest.TestCase):
     '''
@@ -11,21 +16,24 @@ class SourceTest(unittest.TestCase):
       '''
       Set up method that will run before every Test
       '''
-      self.new_source = Source('Ebola out-break scare','https://edition.cnn.com/2019/06/22/health/ebola-outbreak-congo-intl/index.html')from flask import render_template
-from app import app
+      self.new_source = Source('Ebola out-break scare','https://edition.cnn.com/2019/06/22/health/ebola-outbreak-congo-intl/index.html')
 
-@app.errorhandler(404)
-def four_Ow_four(error):
-  '''
-  Function to render the 404 error page
-  '''
-  return render_template('fourOwfour.html'),404
+
+#from flask import render_template
+#from app import app
+
+    #@app.errorhandler(404)
+    def four_Ow_four(error):
+      '''
+      Function to render the 404 error page
+      '''
+      return render_template('fourOwfour.html',404)
 
     def test_isSourceinstance(self):
-      '''
-      Function to test if the object created in the setup is indeed a Source Object
-      '''
+        '''
+        Function to test if the object created in the setup is indeed a Source Object
+        '''
         self.assertTrue(isinstance(self.new_source,Source))
 
 if __name__ == '__main__':
-    unnittest.main()          
+    unittest.main()          
